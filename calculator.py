c8bffb159e7deeda7e7617ec70a8a31c601c2174
dannyCHW <dannydt1697@gmail.com>
2021-07-16 14:36:36 +0800

    FPDF

---
+++ calculator.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/api/discountCalculator/<yearAndFee>")
+def process(yearAndFee=None):
+    x = yearAndFee.split("|")
+    year = int(x[0])
+    fee = int(x[1])
+    
+    if year > 3 :
+        discount = 1-0.3
+    elif year > 2 :
+        discount = 1-0.2
+    elif year > 1 :
+        discount = 1-0.1
+    else :
+        discount = 1
+        
+    
+    return str(fee * discount)
+
+if __name__ == "__main__":
+    app.run(debug=True,
+            host='127.0.0.1',
+            port=8080)
--- verify.php
+++ verify.php
@@ -23,6 +23,38 @@ if(isset($_POST['accept'])){
           </script>";
         }else {
 
+            //
+
+            $sql = "SELECT locationID FROM airwaybill WHERE airWaybillNo = $billNumber ;";
+            $rs = mysqli_query($conn, $sql)or die(mysqli_error($conn));
+            $rc = mysqli_fetch_assoc($rs);
+            $theLocationID = $rc['locationID'];
+
+            $sql = "SELECT rate FROM chargetable WHERE locationID = '$theLocationID' AND  ;";
+            $rs = mysqli_query($conn, $sql)or die(mysqli_error($conn));
+            
+
+
+            if (!extension_loaded("curl")) {
+                die("enable library curl first");
+            }
+              
+              $yearAndFee = "10" . "|" . "123";
+              $url = "http://127.0.0.1:8080/api/discountCalculator/$yearAndFee";   
+              
+              $curl = curl_init($url);
+              curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+              $response = curl_exec($curl);   
+              curl_close($curl);
+              
+              echo "real fee: $response";
+
+            
+
+
+
+            //
+
               $sql ="UPDATE airwaybill SET weight=$weight,staffID = '$stfID' WHERE airWaybillNo=$billNumber";
               $rs = mysqli_query($conn, $sql)or die(mysqli_error($conn));
               $date = date('Y-m-d H:i:s');
